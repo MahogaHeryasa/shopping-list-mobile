4b0fc18cec669e77874986ecaa8b060ac0ea23b8
MahogaHeryasa <hery.heryasa@gmail.com>
2023-11-22 13:19:22 +0700

    Tutorial 8

--- lib/screens/shoplist_form.dart
+++ lib/screens/shoplist_form.dart
@@ -123,7 +123,7 @@ class _ShopFormPageState extends State<ShopFormPage> {
                     if (_formKey.currentState!.validate()) {
                       // Kirim ke Django dan tunggu respons
                       final response = await request.postJson(
-                          "http://mahoga-aribowo-tutorial.pbp.cs.ui.ac.id/create-flutter/",
+                          "http://127.0.0.1:8000/create-flutter/",
                           jsonEncode(<String, String>{
                             'name': _name,
                             'price': _price.toString(),
